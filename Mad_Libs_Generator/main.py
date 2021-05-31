5d932fa8a814442cd99c4bb44a9d2f1e4c774945
Musawenkosi Khulu <71437132+musawenkosikhulu@users.noreply.github.com>
2021-05-31 11:40:11 +0200

    Update main.py

--- Mad_Libs_Generator/main.py
+++ Mad_Libs_Generator/main.py
@@ -26,3 +26,26 @@ while (loop < 10):
 
     # Loop back to "loop = 1"
     loop = loop + 1
+
+    # I wanted to write this in a for loop. So here is my code below.
+    
+    for i in range(10):
+            # All the questions that the program asks the user
+        noun = input("Choose a noun: ")
+        p_noun = input("Choose a plural noun: ")
+        noun2 = input("Choose a noun: ")
+        place = input("Name a place: ")
+        adjective = input("Choose an adjective (Describing word): ")
+        noun3 = input("Choose a noun: ")
+
+        # Displays the story based on the users input
+        print ("------------------------------------------")
+        print ("Be kind to your",noun,"- footed", p_noun)
+        print ("For a duck may be somebody's", noun2,",")
+        print ("Be kind to your",p_noun,"in",place)
+        print ("Where the weather is always",adjective,".")
+        print ()
+        print ("You may think that is this the",noun3,",")
+        print ("Well it is.")
+        print ("------------------------------------------")
+        print(x)
